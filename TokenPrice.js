079dc5bb914e3ef36bec3c3f319584c015be73b9
porus <70854290+GaNa021@users.noreply.github.com>
2022-03-27 18:14:20 +0530

    Add files via upload

---
+++ TokenPrice.js
@@ -0,0 +1,19 @@
+const { ChainId, Fetcher, WETH, Route, Trade, TokenAmount, TradeType } = require ('@uniswap/sdk');
+const ethers = require('ethers');  
+require("dotenv").config();
+
+const customHttpProvider = new ethers.providers.JsonRpcProvider('https://speedy-nodes-nyc.moralis.io/e997d399defafc0668b9c1aa/eth/mainnet');
+
+const chainId = ChainId.MAINNET;
+const tokenAddress = '0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5'
+
+const init = async () => {
+	const dai = await Fetcher.fetchTokenData(chainId, tokenAddress, customHttpProvider);
+	const weth = WETH[chainId];
+	const pair = await Fetcher.fetchPairData(dai, weth, customHttpProvider);
+	const route = new Route([pair], weth);
+	console.log("Mid Price WETH --> DAI:", route.midPrice.toSignificant(6));
+	console.log("Mid Price DAI --> WETH:", route.midPrice.invert().toSignificant(6));
+}
+
+init();
